958ab33d2f37f134c9d36b0187184c6db16c0b3f
Lindaoctaviani7 <lindaoctaviani29@gmail.com>
2026-03-09 02:33:03 +0700

    update js 4 1-2.3(2)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,12 +23,38 @@ class UserController extends Controller
         //UserModel::insert($data); //tambahkan data ke tabel m_user
 
         //tambah data user dengan Eloquent model
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data); //update data user
+        //$data = [
+          //  'level_id' => 2,
+            //'username' => 'manager_tiga',
+            //'nama' => 'Manager 3',
+            //'password' => Hash::make('12345')
+       // ];
+       // UserModel::create($data);
+
         //coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+       // $user = UserModel::all(); //ambil semua data dari tabel m_user
+       // return view('user', ['data' => $user]);
+
+       //$user = UserModel::find(1);
+       //return view('user', ['data' => $user]);
+
+       //$user = UserModel::firstWhere('level_id', 1);
+       //return view('user', ['data' => $user]);
+
+      // $user = UserModel::findOr(20, ['username', 'nama'], function() {
+            //abort(404);
+       //
+
+       //return view('user', ['data' => $user]);
+
+       //$user = UserModel::findOrFail(1);
+       //return view('user', ['data' => $user]);
+
+       //$user = UserModel::where('username', 'manager9')->firstOrFail();
+       //return view('user', ['data' => $user]);
+
+       $user = UserModel::where('level_id', 2)->count();
+       dd($user);
+       return view('user', ['data' => $user]);
     }
 }
